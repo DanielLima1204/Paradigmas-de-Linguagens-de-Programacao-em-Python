c0cc40e33f33031dfe71f5de676b16057b94346c
Daniel Lima <91384776+DanielLima1204@users.noreply.github.com>
2026-08-14 20:10:41 -0300

    Inserindo 2 questao no codigo.

--- Aula-02/testeDeNumero.py
+++ Aula-02/testeDeNumero.py
@@ -5,7 +5,14 @@ def testePositivoOuNegativo(numero: int) -> str:
         return f"O numero {numero} é negativo!"
     else:
         return "O numero é zero!"
+def testeSeParOuImpar(numero: int) -> str:
+    if numero % 2 == 0:
+        return f"O numero {numero} é par"
+    else:
+        return f"O numero {numero} é impar!"
 
 while(True):
     numero = int(input("Digite um numero: "))
     print(testePositivoOuNegativo(numero))
+    print(testeSeParOuImpar(numero))
+
