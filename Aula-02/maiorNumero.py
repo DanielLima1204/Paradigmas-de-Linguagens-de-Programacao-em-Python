1c24177916ed7ccda97830fef73e298136eb4d53
Daniel Lima <91384776+DanielLima1204@users.noreply.github.com>
2026-08-14 20:37:15 -0300

    Modificando a logica para checar numeros iguais.

--- Aula-02/maiorNumero.py
+++ Aula-02/maiorNumero.py
@@ -1,6 +1,11 @@
 num1 = int(input("Digite o 1º numero: "))
 num2 = int(input("Digite o 2º numero: "))
-maior = num1
-if num2 > maior:
+maior = 0
+if num1 > num2:
+    maior = num1
+    print(f"O maior numero é {maior}!")
+elif num2 > num1:
     maior = num2
-print(f"O maior numero é {maior}!")
+    print(f"O maior numero é {maior}!")
+else:
+    print("Os numeros sao iguais!")
