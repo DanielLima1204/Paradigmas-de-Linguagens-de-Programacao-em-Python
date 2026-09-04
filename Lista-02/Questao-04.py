912d39fa150036627c146c01f0a2a0697d1714af
Daniel Lima <91384776+DanielLima1204@users.noreply.github.com>
2026-09-04 18:58:07 -0300

    Inicio Questao 04 Lista 2

---
+++ Lista-02/Questao-04.py
@@ -0,0 +1,4 @@
+valor_financiado = float(input("Digite o valor para financiar: "))
+taxa_mensal = float(input("Digite o valor da taxa mensal (%): "))
+valor_parcela_minima = float(input("Digite o valor da parcela minina: "))
+limite_max_meses = int(input("Digite o limite maximo de meses: "))
